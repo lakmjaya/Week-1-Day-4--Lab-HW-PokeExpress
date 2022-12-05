d7d200525af25fdca1cdf7a128a4b81bce1f836e
DunstWalmart GitHub <lakmjaya@gmail.com>
2022-12-05 02:14:38 -0500

    third

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 app.get('/', function(req, res) {
     res.send('Welcome to the Pokemon App!');
+  });
+
+app.get('/pokemon', function(req, res) {
+    res.render('Index');
   });
 
 app.listen(3000, () => {
