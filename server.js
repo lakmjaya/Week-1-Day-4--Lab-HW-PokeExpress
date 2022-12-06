0b08debafae598490220a55da0fdfc393043d5ec
DunstWalmart GitHub <lakmjaya@gmail.com>
2022-12-05 23:08:40 -0500

    second

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+//require express
 const express = require('express');
+const pokemon = require('./models/pokemon')
+// set express to a variable
 const app = express();
-
+//set a variable of port to 3000
+const port =3000;
+// set view engine
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 app.get('/', function(req, res) {
     res.send('Welcome to the Pokemon App!');
   });
+  
 
 app.get('/pokemon', function(req, res) {
-    res.render('Index');
+    res.render('Index',{pokemonList: pokemon});
   });
 
-app.listen(3000, () => {
-    console.log('listening');
+app.get('/pokemon/:id', function (req,res){
+    res.render('Show', {pokemon:[req.params.id]})
+});
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
 });
--- views/index.jsx
+++ views/index.jsx
@@ -5,11 +5,20 @@ const myStyle = {
 };
   
 class Index extends React.Component {
-       render () {
-        return (
-          
-          <div style={myStyle}><h1>'See All The Pokemon!'</h1></div>
-          
+  render () {
+  const{pokemonList} = this.props
+    return (
+          <div style={myStyle}><h1>'See All The Pokemon!'</h1>
+          <ul>
+            {pokemonList.map((pokemon, i) => {
+              return( 
+                <li>
+                  <a href={`/pokemon/${i}`}>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</a>
+                </li>
+              );
+            })}
+          </ul>
+          </div>  
          );
         }
      }
